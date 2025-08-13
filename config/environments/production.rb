34b5091e3ecbf2af950f25ed84cdfea2e423b5cd
HoseokYoo <hsyoo@pmirnc.com>
2025-08-13 13:55:43 +0900

    fix(config): rb host 추가

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
+  config.hosts << "llm.teamsp.pmirnc.io"
+  config.hosts << "localhost"
+  config.hosts << "0.0.0.0"
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
